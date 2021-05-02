65ff8e443fe1d37d8a69cad97aff204afa8c772a
Ryan <ryandoyle@me.com>
2021-05-02 14:45:07 -0700

    add Nav styles

--- web/src/components/Nav/Nav.js
+++ web/src/components/Nav/Nav.js
@@ -1,14 +1,32 @@
-import { Link, routes } from '@redwoodjs/router'
+import { NavLink, routes } from '@redwoodjs/router'
 
 const Nav = () => {
   return (
     <div className="flex justify-center items-center absolute container w-full bottom-8">
       <nav className="flex justify-between items-center w-2/3 lg:w-1/2 font-display text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold text-purpleLight">
-        <Link to={routes.welcome()}>About</Link>
+        <NavLink
+          to={routes.work()}
+          className="border-b-4 border-transparent hover:border-gold"
+          activeClassName="border-gold"
+        >
+          Work
+        </NavLink>
         <span>|</span>
-        <Link to={routes.work()}>Work</Link>
+        <NavLink
+          to={routes.welcome()}
+          className="border-b-4 border-transparent hover:border-gold"
+          activeClassName="border-gold"
+        >
+          About
+        </NavLink>
         <span>|</span>
-        <Link to={routes.contact()}>Contact</Link>
+        <NavLink
+          to={routes.contact()}
+          className="border-b-4 border-transparent hover:border-gold"
+          activeClassName="border-gold"
+        >
+          Contact
+        </NavLink>
       </nav>
     </div>
   )
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -17,7 +17,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ['hover', 'focus'],
+    },
   },
   plugins: [],
 }
