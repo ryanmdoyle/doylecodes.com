044460f64c93da6d0f1794fa1967db7630e22d56
Ryan <ryandoyle@me.com>
2021-05-02 09:56:20 -0700

    add react-spring

--- web/src/components/SiteHeader/SiteHeader.js
+++ web/src/components/SiteHeader/SiteHeader.js
@@ -1,7 +1,12 @@
+import { useSpring, animated } from 'react-spring'
+
 const SiteHeader = () => {
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
   return (
     <div className="flex justify-center items center">
-      <h1 className="font-display text-8xl font-bold">doyle codes</h1>
+      <animated.h1 style={props} className="font-display text-8xl font-bold">
+        doyle codes
+      </animated.h1>
     </div>
   )
 }
