7644f9931d684e0d2350a687d0b2cc2c4af16ec7
Ryan Doyle <doylecodes@gmail.com>
2021-05-03 16:53:43 -0700

    change header styles

--- web/src/components/SiteHeader/SiteHeader.js
+++ web/src/components/SiteHeader/SiteHeader.js
@@ -11,13 +11,13 @@ const SiteHeader = () => {
     <div className="flex flex-col justify-center items center">
       <animated.h1
         style={h1}
-        className="font-display text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold text-purpleDark text-center my-4 xl:mb-8"
+        className="font-display text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-bold text-purpleLight text-center my-4 xl:mb-8"
       >
         {`Hello, I'm Ryan.`}
       </animated.h1>
       <animated.h2
         style={h2}
-        className="font-display text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold text-purpleLight text-center"
+        className="font-display text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold text-gold text-center"
       >
         Developer, Learner, Educator
       </animated.h2>
