3141c5ec892a287676d56740b165ff2c6ff69216
Ryan <ryandoyle@me.com>
2021-06-01 20:43:33 -0700

    add nodemailer

--- api/src/services/emails/emails.js
+++ api/src/services/emails/emails.js
@@ -1,4 +1,5 @@
 import { db } from 'src/lib/db'
+import nodemailer from 'nodemailer'
 
 export const emails = () => {
   return db.email.findMany()
@@ -10,7 +11,28 @@ export const email = ({ id }) => {
   })
 }
 
-export const createEmail = ({ input }) => {
+export const createEmail = async ({ input }) => {
+  let testAccount = await nodemailer.createTestAccount()
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+      // user: process.env.EMAIL_USER,
+      // pass: process.env.EMAIL_PASS,
+    },
+  })
+
+  await transporter.sendMail({
+    from: input.from, // sender address
+    to: 'ryan@doylecodes.com', // list of receivers
+    subject: input.subject, // Subject line
+    text: input.content, // plain text body
+    html: `<body><p>${input.content}</p></body>`, // html body
+  })
+
   return db.email.create({
     data: input,
   })
