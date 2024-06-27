8586abc5b8e38e95a0b215231aa79579ca976028
Ryan Doyle <ryandoyle@hey.com>
2024-06-27 11:09:38 -0700

    template out cards

---
+++ src/components/ProjectCard.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let title = 'title';
+	export let description = 'description';
+</script>
+
+<div class="card">
+	<h3 class="">{title}</h3>
+	<p class="">{description}</p>
+</div>
+
+<style>
+	.card {
+		max-width: 50vw;
+		height: 20vw;
+		border: 1px solid var(--gray);
+		border-radius: 5px;
+		padding: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,44 @@
-<script lang="ts">
+<script>
+	import ProjectCard from '../components/ProjectCard.svelte';
 	export let data;
-	console.log(data.portfolioData);
+	let portfolios = data.portfolioData;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero-container">
+	<h5 class="">DEVELOPER | EDUCATOR | LEARNER</h5>
+	<h1 class="">Ryan Doyle</h1>
+	<p>
+		Hello! I'm an educator and web developer from Sacramento, CA, with a decade of experience
+		teaching math and science to 6-8th graders. I'm passionate about crafting user-friendly
+		applications and staying updated on the latest web technologies. Here are a few tools I enjoy
+		working with:
+	</p>
+</div>
 
-{#each data.portfolioData as project}
-	<p>{project.properties.Name.title[0]?.text?.content}</p>
-{/each}
+<h2>Projects</h2>
+<div class="cards">
+	{#each portfolios as project}
+		<ProjectCard
+			title={project.properties.Name.title[0]?.text?.content}
+			description={project.properties.Description.rich_text[0].text.content}
+		/>
+	{/each}
+</div>
+
+<style>
+	.hero-container {
+		margin: auto;
+		width: 80%;
+		height: 90vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.cards {
+		width: 100%;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+	}
+</style>
